5e59a91992ec7ac4c2164e5396c4d28379770470
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-07-20 21:10:24 -0500

    chore: setup component testing

---
+++ src/Copyright.spec.tsx
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/experimental-ct-react'
+import Copyright from './Copyright'
+
+test.use({ viewport: { width: 500, height: 500 } })
+
+test('should work', async ({ mount }) => {
+  const component = await mount(<Copyright></Copyright>)
+  await expect(component).toContainText('Your Website')
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     setupFiles: './tests/test-setup.ts',
     exclude: [
       ...configDefaults.exclude,
+      '**/*.spec.tsx', // Component tests
       'tests/**/*', // Playwright tests
     ],
   },
