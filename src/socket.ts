0bba42d797851023b160487bd7e518e375dc04e9
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-07-25 17:39:03 -0500

    fix: socket types

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { Socket } from 'phoenix'
 
-const url = process.env.NEXT_PUBLIC_SOCKET_URL
+const url = process.env.NEXT_PUBLIC_SOCKET_URL || 'ws://localhost:4000/socket'
 
 if (typeof window !== 'undefined') {
   // TODO: save current user to window object to authenticate
@@ -16,9 +16,11 @@ function initializeSocket() {
   return new Socket(url, {
     // TODO: pass optional params for authentication and other purposes on the side of Phoenix
     params: {
-      token: window?.userToken ?? '',
-      currentUser: window?.currentUser ?? '',
-      user_id: window?.location.search.split('=')[1],
+      // @ts-ignore
+      token: window?.userToken || '',
+      // @ts-ignore
+      currentUser: window?.currentUser || '',
+      // user_id: window.location.search.split('=')[1],
     },
     logger: (kind, msg, data) => {
       // console.log(`${kind}: ${msg}`, data)
